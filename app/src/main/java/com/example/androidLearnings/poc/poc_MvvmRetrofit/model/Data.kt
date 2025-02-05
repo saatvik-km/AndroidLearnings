b385d2ea0ec1bf81461b787f61cc8ad3009662e5
Saatvik Krishna M <saatvik.krishna-m@sixt.com>
2025-02-05 20:32:21 +0530

    MVVM_Retrofit_Postreview

--- app/src/main/java/com/example/androidLearnings/poc/poc_MvvmRetrofit/model/Data.kt
+++
@@ -1,5 +0,0 @@
-package com.example.androidLearnings.poc.poc_MvvmRetrofit.model
-
-data class Data(
-    val memes: List<Meme>
-)
--- app/src/main/java/com/example/androidLearnings/poc/poc_MvvmRetrofit/model/Jokes.kt
+++
@@ -1,6 +0,0 @@
-package com.example.androidLearnings.poc.poc_MvvmRetrofit.model
-
-data class Jokes(
-    val `data`: Data,
-    val success: Boolean
-)
